c3b2960f980808b2e333822e3fe2996c39911a71
MinSeob <icdmiracle927@gmail.com>
2023-09-12 05:15:15 +0900

    Feat : 위키 fireabse api 기본 작성

--- src/pages/Wiki.tsx
+++ src/pages/Wiki.tsx
@@ -1,7 +1,48 @@
-import React from 'react';
+import Wikis from '../components/wiki';
+import { db } from '../utils/firebase.config';
+import {
+	collection,
+	deleteDoc,
+	doc,
+	getDocs,
+	setDoc,
+} from 'firebase/firestore';
+
+const getData = async () => {
+	const querySnapshot = await getDocs(collection(db, 'with-collection'));
+	querySnapshot.forEach((doc) => {
+		console.log(doc.id, ' => ', doc.data());
+	});
+};
+
+const setData = async () => {
+	const date = new Date();
+	const dataId = 'with' + date.getTime();
+
+	await setDoc(doc(db, 'with-collection', dataId), {
+		title: 'hello' + dataId,
+	});
+};
+
+const updateData = async (dataId: string) => {
+	const docRef = doc(db, 'with-collection', dataId);
+
+	await setDoc(docRef, {
+		title: 'hello' + dataId,
+	});
+};
+
+const deleteData = async (dataId: string) => {
+	const docRef = doc(db, 'with-collection', dataId);
+	await deleteDoc(docRef);
+};
 
 const Wiki = () => {
-	return <div>Wiki</div>;
+	getData();
+	setData();
+	updateData('with202309114875');
+	// deleteData('with202309114875');
+	return <Wikis />;
 };
 
 export default Wiki;
