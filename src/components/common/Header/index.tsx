f6890936b169b752b90affce6e13997507f9c76a
MinSeob <icdmiracle927@gmail.com>
2023-09-12 16:36:43 +0900

    Feat : assets 폴더 추가 및 svg 파일 추가, Header 러프하게 구현

--- src/components/common/Header/index.tsx
+++ src/components/common/Header/index.tsx
@@ -1,7 +1,32 @@
 import React from 'react';
+import './Header.css';
+import { NavLink } from 'react-router-dom';
+import headerIconSvg from '../../../assets/headerIcon.svg';
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="header">
+      <div className="header__left">
+        <img
+          className="header__left--icon"
+          src={headerIconSvg}
+          alt="headerIcon"
+        />
+        <h1 className="header__left-title">IKUZO</h1>
+      </div>
+      <ul className="header__right">
+        <li className="header__right--item">
+          <NavLink to="/">HOME</NavLink>
+        </li>
+        <li className="header__right--item">
+          <NavLink to="/wiki">WIKI</NavLink>
+        </li>
+        <li className="header__right--item">
+          <NavLink to="/gallery">GALLERY</NavLink>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Header;
