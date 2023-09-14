9e954c9b642d6cb0d66327ac4a36471b190944e2
김다빈 <991012dabin@gmail.com>
2023-09-14 17:39:15 +0900

    etc: 버튼 아이콘 svg 추가

--- src/components/gallery/GalleryItems.tsx
+++ src/components/gallery/GalleryItems.tsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 import { db, storage } from '../../utils/firebase.config';
 import { getData, getDataByField } from '../../utils/util';
+import ReplaceIconSvg from '../../assets/galleryReplaceIcon.svg';
+import DeleteIconSvg from '../../assets/galleryDeleteIcon.svg';
 
 // styled-components
 
@@ -13,43 +15,46 @@ const Item = styled.div`
 
 // img
 const ItemImage = styled.img`
-  content: url('https://www.hotelrestaurant.co.kr/data/photos/20180205/art_15175330519518_43b250.bmp');
   width: 100%;
   height: 100%;
+
+  content: url('https://www.hotelrestaurant.co.kr/data/photos/20180205/art_15175330519518_43b250.bmp');
   object-fit: cover;
+
   transition: all 0.3s;
 `;
 
 // text div
 const ItemHover = styled.div`
-  width: 100%;
-  height: 100%;
-  position: absolute;
-  top: 0;
-  left: 0;
-  background: rgba(0, 0, 0, 0.5);
-
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-items: center;
 
+  width: 100%;
+  height: 100%;
+
+  position: absolute;
+  top: 0;
+  left: 0;
+
+  background: rgba(0, 0, 0, 0.5);
   /* visibility: hidden; */
 `;
 
 // container
 const ItemWrapper = styled.div`
-  margin: 3rem;
-  position: relative;
-  overflow: hidden;
-
-  /* border: 1px solid black; */
-  border-radius: 1rem;
-  box-sizing: border-box;
-
   --length: 15rem;
   width: var(--length);
   height: var(--length);
+
+  position: relative;
+
+  border-radius: 1rem;
+  box-shadow: 2px 3px 5px 0 gray;
+  box-sizing: border-box;
+  margin: 3rem;
+  overflow: hidden;
 
   &:hover ${ItemHover} {
     visibility: visible;
@@ -61,36 +66,43 @@ const ItemWrapper = styled.div`
 `;
 
 const ButtonWrapper = styled.div`
-  margin: 0.3rem;
   display: flex;
   gap: 0.3rem;
+
   position: absolute;
   top: 0;
   right: 0;
+
+  margin: 0.5rem;
 `;
 
 const Button = styled.button`
+  vertical-align: middle;
+
+  font-size: 0.5rem;
+  font-weight: 700;
   border: none;
   border-radius: 1rem;
   outline: none;
   background-color: var(--color-primary);
   cursor: pointer;
   padding: 0.2rem 0.5rem;
-  font-size: 0.5rem;
 `;
 
 const ContentWrapper = styled.div`
-  margin: 1rem;
   width: 100%;
+
   position: absolute;
   bottom: 0;
   left: 0;
+
+  margin: 1rem;
 `;
 
 const Title = styled.h1`
   font-size: 1.8rem;
-  margin-bottom: 0.8rem;
   color: var(--color-white);
+  margin-bottom: 0.8rem;
 `;
 
 const Content = styled.p`
@@ -131,11 +143,19 @@ const GalleryItems: React.FC = () => {
           <Button>
             {/* 나중에 react-icons 설치하고 공유 */}
             {/* <MdPublishedWithChanges /> */}
+            <img
+              src={ReplaceIconSvg}
+              alt="ReplaceIcon"
+            />
             변경
           </Button>
           <Button>
             {/* <TiDeleteOutline /> */}
             {/* <RiDeleteBin2Line /> */}
+            <img
+              src={DeleteIconSvg}
+              alt="ReplaceIcon"
+            />
             삭제
           </Button>
         </ButtonWrapper>
