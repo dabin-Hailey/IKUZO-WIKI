8974800c88403751106a695078f9f46661b996d8
seacrab <seacrab808@naver.com>
2023-09-14 15:07:46 +0900

    Add: 사이드바 css 스타일, 정렬, 라우팅시 메뉴 스타일 추가

--- src/components/common/SidebarGallery/index.tsx
+++ src/components/common/SidebarGallery/index.tsx
@@ -1,49 +1,100 @@
-// SidebarGallery.js
 import React from 'react';
-import { Route, Routes, NavLink } from 'react-router-dom';
+import { Route, Routes, NavLink, useLocation } from 'react-router-dom';
 import Korean from '../../../pages/Korean';
 import Chinese from '../../../pages/Chinese';
 import Japanese from '../../../pages/Japanese';
 import Western from '../../../pages/Western';
+import sidebar from '../../../assets/sidebar.svg';
+import './SidebarGallery.css';
 
 const SidebarGallery = () => {
+  const location = useLocation();
+
+  const isActive = (path: string) => {
+    return location.pathname === path;
+  };
+
   return (
-    <div>
-      <h2>맛집 추천</h2>
-      <nav>
-        <ul>
-          <li>
-            <NavLink to="/gallery">한식</NavLink>
-          </li>
-          <li>
-            <NavLink to="/gallery/chinese">중식</NavLink>
-          </li>
-          <li>
-            <NavLink to="/gallery/japanese">일식</NavLink>
-          </li>
-          <li>
-            <NavLink to="/gallery/western">양식</NavLink>
-          </li>
-        </ul>
-      </nav>
-      <Routes>
-        <Route
-          path="/"
-          element={<Korean />}
-        />
-        <Route
-          path="chinese"
-          element={<Chinese />}
-        />
-        <Route
-          path="japanese"
-          element={<Japanese />}
-        />
-        <Route
-          path="western"
-          element={<Western />}
-        />
-      </Routes>
+    <div className="Wiki">
+      <div className="sidebar__wiki">
+        <div className="sidebar__wiki--header">
+          <img
+            className="sidebar__wiki--icon"
+            src={sidebar}
+            alt="sidebar"
+          />
+          <h2>맛집 추천</h2>
+        </div>
+
+        <nav>
+          <ul>
+            <li>
+              <NavLink
+                to="/gallery/korean"
+                className={`sidebar__wiki--items ${
+                  isActive('/gallery') ? 'active' : ''
+                }`}
+              >
+                한식
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/gallery/chinese"
+                className={`sidebar__wiki--items ${
+                  isActive('/gallery/chinese') ? 'active' : ''
+                }`}
+              >
+                중식
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/gallery/japanese"
+                className={`sidebar__wiki--items ${
+                  isActive('/gallery/japanese') ? 'active' : ''
+                }`}
+              >
+                일식
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/gallery/western"
+                className={`sidebar__wiki--items ${
+                  isActive('/gallery/western') ? 'active' : ''
+                }`}
+              >
+                양식
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div className="content">
+        <Routes>
+          <Route
+            path="/"
+            element={<Korean />}
+          />
+          <Route
+            path="korean"
+            element={<Korean />}
+          />
+          <Route
+            path="chinese"
+            element={<Chinese />}
+          />
+          <Route
+            path="japanese"
+            element={<Japanese />}
+          />
+          <Route
+            path="western"
+            element={<Western />}
+          />
+        </Routes>
+      </div>
     </div>
   );
 };
--- src/components/common/SidebarWiki/index.tsx
+++ src/components/common/SidebarWiki/index.tsx
@@ -1,40 +1,84 @@
 import React from 'react';
-import { Route, Routes, NavLink } from 'react-router-dom';
+import { Route, Routes, NavLink, useLocation } from 'react-router-dom';
 import Notice from '../../../pages/Notice';
 import Intro from '../../../pages/Intro';
 import With from '../../../pages/With';
+import sidebar from '../../../assets/sidebar.svg';
+import './SidebarWiki.css';
 
 const SidebarWiki = () => {
+  const location = useLocation();
+
+  const isActive = (path: string) => {
+    return location.pathname === path;
+  };
+
   return (
-    <div>
-      <h2>위키</h2>
-      <nav>
-        <ul>
-          <li>
-            <NavLink to="/wiki">공지사항</NavLink>
-          </li>
-          <li>
-            <NavLink to="/wiki/intro">소개</NavLink>
-          </li>
-          <li>
-            <NavLink to="/wiki/with">같이 먹을 사람 구해요</NavLink>
-          </li>
-        </ul>
-      </nav>
-      <Routes>
-        <Route
-          path="/"
-          element={<Notice />}
-        />
-        <Route
-          path="intro"
-          element={<Intro />}
-        />
-        <Route
-          path="with"
-          element={<With />}
-        />
-      </Routes>
+    <div className="Wiki">
+      <div className="sidebar__wiki">
+        <div className="sidebar__wiki--header">
+          <img
+            className="sidebar__wiki--icon"
+            src={sidebar}
+            alt="sidebar"
+          />
+          <h2>Wiki</h2>
+        </div>
+        <nav>
+          <ul>
+            <li>
+              <NavLink
+                to="/wiki/notice"
+                className={`sidebar__wiki--items ${
+                  isActive('/wiki') ? 'active' : ''
+                }`}
+              >
+                공지사항
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/wiki/intro"
+                className={`sidebar__wiki--items ${
+                  isActive('/wiki/intro') ? 'active' : ''
+                }`}
+              >
+                소개
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/wiki/with"
+                className={`sidebar__wiki--items ${
+                  isActive('/wiki/with') ? 'active' : ''
+                }`}
+              >
+                같이 먹을 사람 구해요
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div className="content">
+        <Routes>
+          <Route
+            path="/"
+            element={<Notice />}
+          />
+          <Route
+            path="notice"
+            element={<Notice />}
+          />
+          <Route
+            path="intro"
+            element={<Intro />}
+          />
+          <Route
+            path="with"
+            element={<With />}
+          />
+        </Routes>
+      </div>
     </div>
   );
 };
