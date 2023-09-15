ce17810890fe4bcebeeca1f9c299822d18a982df
lilviolie <lilviolie@gmail.com>
2023-09-15 21:55:22 +0900

    Feat: 맛집 추가하기 버튼 추가

--- src/components/common/SidebarGallery/index.tsx
+++ src/components/common/SidebarGallery/index.tsx
@@ -22,6 +22,8 @@ const SidebarWikiWrapper = styled.div`
   border-radius: 1rem;
   color: var(--color-black);
   box-shadow: 0 0.2rem 0.4rem 0 #c3c3c3;
+  display: flex;
+  flex-direction: column;
 `;
 
 const SidebarHeader = styled.div`
@@ -34,6 +36,10 @@ const SidebarHeader = styled.div`
 const SidebarIcon = styled.img`
   width: 2rem;
   color: #ffa800;
+`;
+
+const SidebarNav = styled.nav`
+  flex-grow: 1;
 `;
 
 const SidebarNavLink = styled(NavLink)`
@@ -56,6 +62,17 @@ const SidebarNavLink = styled(NavLink)`
   }
 `;
 
+const AddButton = styled.button`
+  width: 190px;
+  height: 35px;
+  background-color: var(--color-primary);
+
+  border: none;
+  border-radius: 30px;
+  box-shadow: 0 2px 4px #9c9c9c;
+  cursor: pointer;
+`;
+
 const ContentWrapper = styled.div`
   margin-left: 23rem;
   margin-top: 7rem;
@@ -72,7 +89,7 @@ const SidebarGallery = () => {
           />
           <h2>맛집 추천</h2>
         </SidebarHeader>
-        <nav>
+        <SidebarNav>
           <ul>
             <li>
               <SidebarNavLink to="/gallery/korean">한식</SidebarNavLink>
@@ -87,7 +104,8 @@ const SidebarGallery = () => {
               <SidebarNavLink to="/gallery/western">양식</SidebarNavLink>
             </li>
           </ul>
-        </nav>
+        </SidebarNav>
+        <AddButton>맛집 추가하기</AddButton>
       </SidebarWikiWrapper>
       <ContentWrapper>
         <Routes>
