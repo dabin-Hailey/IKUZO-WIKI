605b39b7ef01bdc2ec2175246e50d8c5afbc99e6
MinSeobKim <icdmiracle927@gmail.com>
2023-09-15 18:06:16 +0900

    Fix : 목업데이터 생성 함수 제거

--- src/pages/With.tsx
+++ src/pages/With.tsx
@@ -6,16 +6,6 @@ export interface Root {
   id: string;
 }
 
-const currentUnixTime = Math.floor(new Date().getTime() / 1000);
-const MockupData = {
-  title: '여기산 맛집 가실분?',
-  contents: '수원에서 제일 맛있는 곳인데 같이 가실 분 구해요',
-  location: '수원시 팔달구',
-  time: currentUnixTime,
-  joined: 1,
-  people: 3,
-};
-
 const With = (): JSX.Element => {
   const [datas, setDatas] = useState<Root[]>([]);
 
@@ -29,7 +19,6 @@ const With = (): JSX.Element => {
 
     fetchData();
 
-    setData('with-collection', MockupData);
     getDataBySnapshot('with-collection', setDatas);
   }, []);
 
