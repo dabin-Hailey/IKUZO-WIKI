ee5d7022a33e6a4d9f4d75334d023c3110c6a279
lilviolie <lilviolie@gmail.com>
2023-09-22 13:52:01 +0900

    Design: wiki 페이지 공통 레이아웃 분리

---
+++ src/components/wiki/Layout/WikiLayout.styled.tsx
@@ -0,0 +1,60 @@
+import styled from 'styled-components';
+
+export const WikiContainer = styled.div`
+  box-sizing: border-box;
+  width: 100%;
+  max-width: 1070px;
+  margin: 0 0 0 5%;
+  height: 79vh;
+`;
+
+export const WikiHeader = styled.div`
+  padding-top: 10px;
+  display: flex;
+  justify-content: space-between;
+
+  height: 70px;
+  border-bottom: 3px solid var(--color-orange);
+`;
+
+export const Title = styled.div`
+  font-family: 'IBMPlexSansKR-Regular';
+  font-size: 3rem;
+  font-weight: 700;
+`;
+
+export const Primary = styled.span`
+  color: var(--color-orange);
+`;
+
+export const WikiButton = styled.button`
+  font-family: 'SBAggroB';
+  width: 7rem;
+  height: 3rem;
+  background-color: var(--color-primary);
+  color: var(--color-white);
+
+  border: none;
+  border-radius: 30px;
+  cursor: pointer;
+
+  transform: translateY(1px);
+  transition: 0.2s;
+
+  &:hover {
+    transform: translateY(3px);
+    background-color: var(--color-btn);
+  }
+`;
+
+export const Contents = styled.div`
+  box-sizing: border-box;
+  width: 100%;
+  height: 60vh;
+  margin-top: 50px;
+  padding: 20px 30px 40px;
+
+  border-radius: 15px;
+  background-color: #f2f2f2;
+  overflow: auto;
+`;
